337ea10fdd4dd3a4de9b73f043e163edb893f5cb
spalamarchuk <spalamarchuk@homeaway.com>
2017-10-07 01:01:00 +0300

    Simplification

--- src/main/java/de/thomaskrille/dropwizard_template_config/DelegateProviderMap.java
+++ src/main/java/de/thomaskrille/dropwizard_template_config/DelegateProviderMap.java
@@ -1,26 +1,25 @@
 package de.thomaskrille.dropwizard_template_config;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 import com.google.common.collect.ForwardingMap;
 
-public class DelegateProviderMap extends ForwardingMap<String, Object> {
+class DelegateProviderMap extends ForwardingMap<String, Object> {
 
-    private final Map<String, TemplateConfigVariablesProvider> providers;
+    private final TemplateConfigVariablesProvider[] providers;
 
     public DelegateProviderMap(TemplateConfigVariablesProvider... providers) {
-        this.providers = new LinkedHashMap<>();
-        Stream.of(providers).forEach(p -> DelegateProviderMap.this.providers.put(p.getNamespace(), p));
+        this.providers = Objects.requireNonNull(providers);
     }
 
     @Override
     protected Map<String, Object> delegate() {
         Map<String, Object> delegate = new HashMap<>();
-        providers.forEach((k, v) -> delegate.put(k, v.getVariables()));
-        providers.values().stream().map(TemplateConfigVariablesProvider::getVariables).forEach(delegate::putAll);
+        Stream.of(providers).forEach(p -> delegate.put(p.getNamespace(), p.getVariables()));
+        Stream.of(providers).map(TemplateConfigVariablesProvider::getVariables).forEach(delegate::putAll);
         return delegate;
     }
 
